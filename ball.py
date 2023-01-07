b083ffe51cda2f5d74be9dd9328fc5250b145479
alon <alonsuissa12@gmail.com>
2023-01-08 00:50:46 +0200

    flipping ball

--- ball.py
+++ ball.py
@@ -10,20 +10,29 @@ class Ball:
         self.img = pygame.transform.scale(temp_img, (temp_img.get_width() * scale, temp_img.get_height() * scale))
         self.gravity = gravity
         self.floor = floor
-        self.x_change = 5
+        self.x_change = 4
         self.y_change = 0
+        self.fliper = 0
 
     def ball_movement(self, x_change, y_change):
         self.x_change = x_change
         self.y_change = y_change
-        self.boundaries()
+
 
     def draw(self):
         self.y_change = self.gravity + self.y_change
-        self.x_change = self.x_change * 0.98
+        self.x_change = self.x_change * 0.995
         self.x += self.x_change
         self.y += self.y_change
         self.boundaries()
+        if self.x_change >= 2 or self.x_change <= -2:
+            self.fliper = (self.fliper + int(self.x_change*2))
+        elif (2 > self.x_change > 0.1) or (-2 < self.x_change < -0.1):
+            self.fliper = self.fliper + 2
+        elif 0.1 < self.x < 0.3 or -0.1 > self.x > -0.3:
+            self.fliper = self.fliper + 1
+        self.img = pygame.transform.flip(self.img, self.fliper >= 9, self.fliper >= 9)
+        self.fliper %= 10
         self.screen.blit(self.img, (self.x, self.y))
 
     def boundaries(self):
@@ -38,4 +47,4 @@ class Ball:
             self.x_change = (-1 * self.x_change) / 1.5
         if self.x < 60:
             self.x = 60
-            self.x_change = (-1 * self.x_change) / 2
+            self.x_change = (-1 * self.x_change) / 1.5
